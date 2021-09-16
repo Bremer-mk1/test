65a3f483f1a91255760c9ff842b6c56b1b21b013
sonssy <49086847+sonssy@users.noreply.github.com>
2021-09-17 00:57:15 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -20,13 +20,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //mp3파일재생방법은 2가지인듯함
+        //1번째는 SoundPool 사용 -> 짧은 효과음등을 재생할때 사용
+        //2번째는 MediaPlayer 사용 -> SoundPool보다 긴 파일을 재생할때 사용
+        
+        //onCreate시에 바로 재생하게 했지만 SoundPool의 경우 소리가 중간에 끊어짐 안됨
+        sound_test();
+        //onCreate시에 바로 재생하게 했지만 MediaPlayer의경우 안됨
         media_sound_test();
         destory_sound();
+        //앱의 최초 onCreate시 로드가 제대로 안되서 안되는듯?
 
         test_btn= findViewById(R.id.button);
         test_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                
+                //클릭시 SoundPool테스트 결과 -> 문제없이 사용가능
                 sound_test();
                 Intent intent = new Intent(MainActivity.this, page1.class);
                 startActivity(intent);
@@ -34,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //1번째 SoundPool 사용-> 샘플로 보내준 파일 재생가능함 안끊어짐
     private void sound_test(){
         SoundPool soundPool;
         int soundID;
@@ -49,11 +60,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    
+//2번째 MediaPlayer 사용-> 샘플로 보내준 파일 재생가능함 안끊어짐
     private void media_sound_test() {
         mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.test);
         mediaPlayer.start();
     }
+    
+    //2번째 MediaPlayer 사용의 경우 사용 후 릴리즈 해줘야됨
     private void destory_sound(){
         if(mediaPlayer != null){
             mediaPlayer.release();
